ba4d2efcf56e180dcc90ca66b0441080ae7f3dd5
derrick b <dbozich1@gmail.com>
2018-11-02 16:41:50 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,6 +4,11 @@ export default function manageTodo(state = {
   switch (action.type) {
     case 'ADD_TODO':
 
+    const todo = {
+      id: Math.random()*10000000000000000,
+      text: action.payload.text
+    }
+
       return { todos: state.todos.concat(action.payload.text) };
 
     case 'DELETE_TODO':
