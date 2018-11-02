aa2d2237ccc65bc475e3fa334cc05e53e03870cf
derrick b <dbozich1@gmail.com>
2018-11-02 16:47:57 +0000

    automatically backed up by learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,7 +4,7 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo) => <Todo key={todo.id} delete={this.props.delete} text={todo.text} />)
+  renderTodos = () => this.props.todos.map((todo) => <Todo key={todo.id} delete={this.props.delete} todo={todo} />)
 
   render() {
     return(
